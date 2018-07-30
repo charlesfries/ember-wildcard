6ed7e55a3c52c3d68bf4b012e560d91737aa594d
Charles Fries <charliefries@icloud.com>
2018-07-29 22:15:49 -0700

    Added comparison method

--- addon/services/compare.js
+++ addon/services/compare.js
@@ -1,4 +1,30 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  symbol: null,
+
+  init() {
+    this._super(...arguments);
+
+    this.set('symbol', '*');
+  },
+
+  setSymbol(symbol) {
+    if (typeof symbol !== 'string') return;
+
+    this.set('symbol', symbol);
+  },
+
+  wildcard(actual, expect) {
+    if (actual.length !== expect.length) return false;
+    let a = '';
+    let e = '';
+    for (let i = 0; i < expect.length; i++) {
+      if (expect[i] !== this.get('symbol')) {
+        a += actual[i];
+        e += expect[i];
+      }
+    }
+    return a === e;
+  }
 });
