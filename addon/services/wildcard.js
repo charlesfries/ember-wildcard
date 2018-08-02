a8b6d01a8984745934344bd57be9267e04c8685e
Charles Fries <charliefries@icloud.com>
2018-08-01 17:56:51 -0700

    Added alternate method

--- addon/services/wildcard.js
+++ addon/services/wildcard.js
@@ -24,5 +24,18 @@ export default Service.extend({
       }
     }
     return a === e;
-  }
+  },
+
+  // match(opening, closing, actual, expect) {
+  //   if (actual.length !== expect.length) return false;
+  //   let a = '';
+  //   let e = '';
+  //   for (let i = 0; i < expect.length; i++) {
+  //     if (expect[i] !== this.get('symbol')) {
+  //       a += actual[i];
+  //       e += expect[i];
+  //     }
+  //   }
+  //   return a === e;
+  // }
 });
